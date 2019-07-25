3dccf7805388cb4e9d2fa3c48ec3d5df3b9f10ca
Carlee <47862582+CarleeSinton@users.noreply.github.com>
2019-07-25 13:12:17 -0700

    Add files via upload

---
+++ Mod1_Lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "";
+            string lastName = "";
+            int age = 0;
+            string street = "";
+            string city = "";
+            string country = "";
+            DateTime birthDate;
+
+            firstName = "C";
+            lastName = "S";
+            age = 57;
+            street = "100 Main Street";
+            city = "Boston";
+            country = "USA";
+            birthDate = new DateTime(1867, 4, 18);
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine("{0} years old.", age);
+            Console.WriteLine(street + ", " + city + ", " + country);
+            Console.WriteLine($"Born on {birthDate}");
+
+        }
+    }
+}
